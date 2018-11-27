0d5cf565d7c8368e7e26eda355ab0e1ec940f6c0
Dorian Gentine <dorian.gentine@mail.novancia.fr>
2018-11-27 09:59:43 +0700

    push test

--- app/javascript/components/carouselGrafikart.js
+++ app/javascript/components/carouselGrafikart.js
@@ -1,3 +1,81 @@
+/**
+ * Permet de rajouter la navigation tactile pour le carousel
+ */
+class CarouselTouchPlugin {
+
+  /**
+   * @param {Carousel} carousel
+   */
+  constructor (carousel) {
+    carousel.container.addEventListener('dragstart', e => e.preventDefault())
+    carousel.container.addEventListener('mousedown', this.startDrag.bind(this))
+    carousel.container.addEventListener('touchstart', this.startDrag.bind(this))
+    window.addEventListener('mousemove', this.drag.bind(this))
+    window.addEventListener('touchmove', this.drag.bind(this))
+    window.addEventListener('touchend', this.endDrag.bind(this))
+    window.addEventListener('mouseup', this.endDrag.bind(this))
+    window.addEventListener('touchcancel', this.endDrag.bind(this))
+    this.carousel = carousel
+  }
+
+  /**
+   * Démarre le déplacement au touché
+   * @param {MouseEvent|TouchEvent} e
+   */
+  startDrag (e) {
+    if (e.touches) {
+      if (e.touches.length > 1) {
+        return
+      } else {
+        e = e.touches[0]
+      }
+    }
+    this.origin = {x: e.screenX, y: e.screenY}
+    this.width = this.carousel.containerWidth
+    this.carousel.disableTransition()
+  }
+
+  /**
+   * Déplacement
+   * @param {MouseEvent|TouchEvent} e
+   */
+  drag (e) {
+    if (this.origin) {
+      let point = e.touches ? e.touches[0] : e
+      let translate = {x: point.screenX - this.origin.x, y: point.screenY - this.origin.y}
+      if (e.touches && Math.abs(translate.x) > Math.abs(translate.y)) {
+        e.preventDefault()
+        e.stopPropagation()
+      }
+      let baseTranslate = this.carousel.currentItem * -100 / this.carousel.items.length
+      this.lastTranslate = translate
+      this.carousel.translate(baseTranslate + 100 * translate.x / this.width)
+    }
+  }
+
+  /**
+   * Fin du déplacement
+   * @param {MouseEvent|TouchEvent} e
+   */
+  endDrag (e) {
+    if (this.origin && this.lastTranslate) {
+      this.carousel.enableTransition()
+      if (Math.abs(this.lastTranslate.x / this.carousel.carouselWidth) > 0.2) {
+        if (this.lastTranslate.x < 0) {
+          this.carousel.next()
+        } else {
+          this.carousel.prev()
+        }
+      } else {
+        this.carousel.gotoItem(this.carousel.currentItem)
+      }
+    }
+    this.origin = null
+  }
+
+
+}
+
 class Carousel {
   /**
    * This callback type is called `requestCallback` and is displayed as a global symbol.
@@ -12,6 +90,7 @@ class Carousel {
    * @param {Object} [options.slidesToScroll=1] Nombre d'éléments à faire défiler
    * @param {Object} [options.slidesVisible=1] Nombre d'éléments visible dans un slide
    * @param {boolean} [options.loop=false] Doit-t-on boucler en fin de carousel
+   * @param {boolean} [options.infinite=false]
    * @param {boolean} [options.pagination=false]
    * @param {boolean} [options.navigation=true]
    */
@@ -22,12 +101,17 @@ class Carousel {
       slidesVisible: 1,
       loop: false,
       pagination: false,
-      navigation: true
+      navigation: true,
+      infinite: false
     }, options)
+    if (this.options.loop && this.options.infinite) {
+      throw new Error('Un carousel ne peut être à la fois en boucle et en infinie')
+    }
     let children = [].slice.call(element.children)
     this.isMobile = false
     this.currentItem = 0
     this.moveCallbacks = []
+    this.offset = 0
 
     // Modification du DOM
     this.root = this.createDivWithClass('carousel')
@@ -38,9 +122,21 @@ class Carousel {
     this.items = children.map((child) => {
       let item = this.createDivWithClass('carousel__item')
       item.appendChild(child)
-      this.container.appendChild(item)
       return item
     })
+    if (this.options.infinite) {
+      this.offset = this.options.slidesVisible + this.options.slidesToScroll
+      if (this.offset > children.length) {
+        console.error("Vous n'avez pas assez d'élément dans le carousel", element)
+      }
+      this.items = [
+        ...this.items.slice(this.items.length - this.offset).map(item => item.cloneNode(true)),
+        ...this.items,
+        ...this.items.slice(0, this.offset).map(item => item.cloneNode(true)),
+      ]
+      this.gotoItem(this.offset, false)
+    }
+    this.items.forEach(item => this.container.appendChild(item))
     this.setStyle()
     if (this.options.navigation) {
       this.createNavigation()
@@ -50,7 +146,7 @@ class Carousel {
     }
 
     // Evenements
-    this.moveCallbacks.forEach(cb => cb(0))
+    this.moveCallbacks.forEach(cb => cb(this.currentItem))
     this.onWindowResize()
     window.addEventListener('resize', this.onWindowResize.bind(this))
     this.root.addEventListener('keyup', e => {
@@ -60,6 +156,10 @@ class Carousel {
         this.prev()
       }
     })
+    if (this.options.infinite) {
+      this.container.addEventListener('transitionend', this.resetInfinite.bind(this))
+    }
+    new CarouselTouchPlugin(this)
   }
 
   /**
@@ -105,19 +205,24 @@ class Carousel {
     let pagination = this.createDivWithClass('carousel__pagination')
     let buttons = []
     this.root.appendChild(pagination)
-    for (let i = 0; i < this.items.length; i = i + this.options.slidesToScroll) {
+    for (let i = 0; i < (this.items.length - 2 * this.offset); i = i + this.options.slidesToScroll) {
       let button = this.createDivWithClass('carousel__pagination__button')
-      button.addEventListener('click', () => this.gotoItem(i))
+      button.addEventListener('click', () => this.gotoItem(i + this.offset))
       pagination.appendChild(button)
       buttons.push(button)
     }
     this.onMove(index => {
-      let activeButton = buttons[Math.floor(index / this.options.slidesToScroll)]
+      let count = this.items.length - 2 * this.offset
+      let activeButton = buttons[Math.floor(((index - this.offset) % count) / this.options.slidesToScroll)]
       if (activeButton) {
         buttons.forEach(button => button.classList.remove('carousel__pagination__button--active'))
         activeButton.classList.add('carousel__pagination__button--active')
       }
     })
+  }
+
+  translate (percent) {
+    this.container.style.transform = 'translate3d(' + percent + '%, 0, 0)'
   }
 
   /**
@@ -134,8 +239,9 @@ class Carousel {
   /**
    * Déplace le carousel vers l'élément ciblé
    * @param {number} index
+   * @param {boolean} [animation = true]
    */
-  gotoItem (index) {
+  gotoItem (index, animation = true) {
     if (index < 0) {
       if (this.options.loop) {
         index = this.items.length - this.slidesVisible
@@ -150,9 +256,27 @@ class Carousel {
       }
     }
     let translateX = index * -100 / this.items.length
-    this.container.style.transform = 'translate3d(' + translateX + '%, 0, 0)'
+    if (animation === false) {
+      this.disableTransition()
+    }
+    this.translate(translateX)
+    this.container.offsetHeight // force repaint
+    if (animation === false) {
+      this.enableTransition()
+    }
     this.currentItem = index
     this.moveCallbacks.forEach(cb => cb(index))
+  }
+
+  /**
+   * Déplace le container pour donner l'impression d'un slide infini
+   */
+  resetInfinite () {
+    if (this.currentItem <= this.options.slidesToScroll) {
+      this.gotoItem(this.currentItem + (this.items.length - 2 * this.offset), false)
+    } else if (this.currentItem >= this.items.length - this.offset) {
+      this.gotoItem(this.currentItem - (this.items.length - 2 * this.offset), false)
+    }
   }
 
   /**
@@ -186,6 +310,14 @@ class Carousel {
     return div
   }
 
+  disableTransition () {
+    this.container.style.transition = 'none'
+  }
+
+  enableTransition () {
+    this.container.style.transition = ''
+  }
+
   /**
    * @returns {number}
    */
@@ -200,17 +332,29 @@ class Carousel {
     return this.isMobile ? 1 : this.options.slidesVisible
   }
 
+  /**
+   * @returns {number}
+   */
+  get containerWidth () {
+    return this.container.offsetWidth
+  }
+
+  /**
+   * @returns {number}
+   */
+  get carouselWidth () {
+    return this.root.offsetWidth
+  }
+
 }
 
 let onReady = function () {
 
-  console.log("Hello")
   new Carousel(document.querySelector('#carousel_portofolio'), {
       slidesVisible: 3,
       slidesToScroll: 1,
-      loop: true
+      infinite: true
   })
-
 
 }
 
