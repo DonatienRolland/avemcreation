63c15b2a0895949c525fabb12f4e32149154b90f
Dorian Gentine <dorian.gentine@mail.novancia.fr>
2019-03-13 14:47:53 +0700

    enlever js scroll

--- app/javascript/components/forceScroll.js
+++ app/javascript/components/forceScroll.js
@@ -1,48 +1,20 @@
-var scrollStop = require('../components/scrollStop').scrollStop
+// var scrollStop = require('../components/scrollStop').scrollStop
 
 // CODE FONCTIONNEL
-$(document).ready(function(){
-  // var scrollIni = 0;
-  let mobile = window.innerWidth < 800
-  if (!mobile) {
-    scrollStop(function () {
-      var ecranActif = Math.trunc( ($(window).scrollTop() + (screen.availHeight / 2) ) / screen.availHeight );
-      var ecrans = $(".ecran");
-      var ecranID = ecrans[ecranActif].id
-      var ecran = $("#"+ecranID)
-      $("html, body").animate({
-        scrollTop: ecran.offset().top
-      }, 250);
-    });
-  }
+// $(document).ready(function(){
+//   // var scrollIni = 0;
+//   let mobile = window.innerWidth < 800
+//   if (!mobile) {
+//     scrollStop(function () {
+//       var ecranActif = Math.trunc( ($(window).scrollTop() + (screen.availHeight / 2) ) / screen.availHeight );
+//       var ecrans = $(".ecran");
+//       var ecranID = ecrans[ecranActif].id
+//       var ecran = $("#"+ecranID)
+//       $("html, body").animate({
+//         scrollTop: ecran.offset().top
+//       }, 250);
+//     });
+//   }
 
 
-})
-
-    // $(window).scroll(function(){
-    //   var scrollAim = $(this).scrollTop();
-    //   var ecranActif = Math.trunc( $(window).scrollTop() / screen.availHeight );
-    //   if( Math.abs(scrollAim - scrollIni) > 100){
-    //     var ecrans = $(".ecran");
-    //     var ecranActif = Math.trunc( $(window).scrollTop() / screen.availHeight );
-    //     if(scrollAim > scrollIni){
-    //       if(ecranActif != ecrans.length){
-    //         var ecranID = ecrans[ecranActif + 1].id
-    //         var ecran = $("#"+ecranID)
-    //         $("html, body").animate({
-    //           scrollTop: ecran.offset().top
-    //         }, 250);
-    //       }
-    //     } else if(scrollAim < scrollIni){
-    //       var ecranID = ecrans[ecranActif].id
-    //       var ecran = $("#"+ecranID)
-    //       $("html, body").animate({
-    //         scrollTop: ecran.offset().top
-    //       }, 250);
-    //     }
-    //     scrollIni = $(window).scrollTop();
-    //   }
-    // })
-
-
-// $(document).scrollTop(ecran.next.offset().top);
+// })
