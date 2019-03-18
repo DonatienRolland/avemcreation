b43d32533b2d2499876b9c9ba5fb5b83a0af494e
donatienrolland <donatien@rollandmail.com>
2019-03-18 15:22:21 +0700

    change text les

--- app/javascript/components/dynamicNavbar.js
+++ app/javascript/components/dynamicNavbar.js
@@ -1,44 +1,4 @@
-$(document).ready(function() {
-  $(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    var un = $("#contact").offset().top - 195;
 
-    // navbar-wagon-right becoming transparent
-    $(".navbar-wagon-right").css("opacity", ( 1 - ( scroll / 100 )));
-
-    //logo dynamic change
-    var x = scroll - un;
-    if(x < 0){
-      x = 0;
-      $(".logo-gris").css("transform", "translateY(" + x + "%)");
-      $(".dynamic-logo-item-gris").css("transform", "translateY(-" + x + "%)");
-      $(".logo-blanc").css("transform", "translateY(-" + ( 100 - x ) + "%)");
-      $(".dynamic-logo-item-blanc").css("transform", "translateY(" + (100 - x ) + "%)");
-    } else if ( x > 100 ){
-      x = 100;
-      $(".logo-gris").css("transform", "translateY(" + x + "%)");
-      $(".dynamic-logo-item-gris").css("transform", "translateY(-" + x + "%)");
-      $(".logo-blanc").css("transform", "translateY(-" + ( 100 - x ) + "%)");
-      $(".dynamic-logo-item-blanc").css("transform", "translateY(" + (100 - x ) + "%)");
-    } else {
-      $(".logo-gris").css("transform", "translateY(" + x + "%)");
-      $(".dynamic-logo-item-gris").css("transform", "translateY(-" + x + "%)");
-      $(".logo-blanc").css("transform", "translateY(-" + ( 100 - x ) + "%)");
-      $(".dynamic-logo-item-blanc").css("transform", "translateY(" + (100 - x ) + "%)");
-    }
-
-    //navbar background to transparent
-    if(scroll >= un){
-      $("#navbar-white-bg-bottom").removeClass("hidden");
-      $(".navbar-white-bg").css("background-color", "transparent");
-      // $(".navbar-wagon-brand").children().attr("src", "assets/Logo-blanc-detoure.png")
-    } else {
-      $("#navbar-white-bg-bottom").addClass("hidden");
-      $(".navbar-white-bg").css("background-color", "white");
-      // $(".navbar-wagon-brand").children().attr("src", "assets/Logo-gris-detoure.png")
-    }
-  })
-});
 
 
 // $(document).ready(function() {
