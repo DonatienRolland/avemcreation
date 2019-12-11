36873128ed41b1f1d54ffd2aefef1de267ffcb73
erwan guillou <erwan@avemcreation.com>
2019-12-11 19:27:35 +0100

    Avem.V2.02

--- js/ga.js
+++ js/ga.js
@@ -1,15 +1,15 @@
 // Tracker Google analytics
 
-    var ga_fired = false;
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+//     var ga_fired = false;
+//         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+//         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+//         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+//         })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-        ga('create', 'UA-131037777-1', 'auto');
-        ga('set', 'anonymizeIp', true);
-        ga('send', 'pageview');
-        ga_fired = true;
+//         ga('create', 'UA-131037777-1', 'auto');
+//         ga('set', 'anonymizeIp', true);
+//         ga('send', 'pageview');
+//         ga_fired = true;
 
 
 // Hotjar Tracking Code for https://www.avemcreation.com/
