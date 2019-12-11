c15dcb9a8de5809114054556f33e92e58835bea0
erwan guillou <erwan@avemcreation.com>
2019-12-11 18:40:55 +0100

    Avem.V2.02

--- js/ga.js
+++ js/ga.js
@@ -1,13 +1,15 @@
 // Tracker Google analytics
 
-   
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+    var ga_fired = false;
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  gtag('config', 'UA-131037777-1');
-
-
+        ga('create', 'UA-131037777-1', 'auto');
+        ga('set', 'anonymizeIp', true);
+        ga('send', 'pageview');
+        ga_fired = true;
 
 
 // Hotjar Tracking Code for https://www.avemcreation.com/
